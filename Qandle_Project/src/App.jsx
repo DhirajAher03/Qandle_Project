2e0a6fdf7723131115422509e2c88dcce975c35e
Dhiraj Aher <dhirajaheraher@gmail.com>
2025-04-08 12:11:12 +0530

    Navbar Completed

--- Qandle_Project/src/App.jsx
+++ Qandle_Project/src/App.jsx
@@ -1,22 +1,23 @@
 
 import './App.css'
-import LeaveComponent from './Components/Leave/LeaveComponent'
 import LoginComponent from './Components/Login/LoginComponent'
 import NavbarComponent from './Components/Navbar/NavbarComponent'
 import Overview from './Components/Overview/Overview'
 import SidebarComponent from './Components/Slidebar/SidebarComponent'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import { Col, Row } from 'react-bootstrap'
 
 function App() {
 
   return (
     <>
+    <LoginComponent/>
       <BrowserRouter>
         <Row lg={1}>
           <NavbarComponent />
         </Row>
-        <Row lg={11} className='custom-row'>
+        <Row lg={11} className="custom-row">
           <Col lg={3}>
             <SidebarComponent />
           </Col>
