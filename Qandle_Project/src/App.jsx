03100d358ef45f06a516003bc7eb898cedfdaa10
Shubham Aher <skaedu6502@gmail.com>
2025-04-08 10:07:57 +0530

    Slidebar Completed

--- Qandle_Project/src/App.jsx
+++ Qandle_Project/src/App.jsx
@@ -1,12 +1,20 @@
-// import { useState } from 'react'
+
 import './App.css'
 import NavbarComponent from './Components/Navbar/NavbarComponent'
+import SidebarComponent from './Components/Slidebar/SidebarComponent'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
       <NavbarComponent />
+      <SidebarComponent/>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ Qandle_Project/src/Components/Slidebar/SidebarComponent.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import { Col, NavLink, Row } from 'react-bootstrap'
+import sumagoIcon from '../../assets/Sumago_logo.png'
+import './SidebarComponent.css'
+import ProfilePic from '../../assets/Profile_pic.webp'
+import { SlHome, SlPeople,SlCalender   } from "react-icons/sl";
+import { IoAlarmOutline,IoKeyOutline,IoExitOutline } from "react-icons/io5";
+import { LuFileSpreadsheet } from "react-icons/lu";
+import { MdPendingActions, MdOutlinePersonSearch,MdDevicesOther } from "react-icons/md";
+import { FiTrendingUp } from "react-icons/fi";
+import { TbTopologyRing } from "react-icons/tb";
+import { CiLogout } from "react-icons/ci";
+import { FaRegBell } from "react-icons/fa";
+import { FaPeopleRobbery } from "react-icons/fa6";
+import { GiLevelFourAdvanced } from "react-icons/gi";
+
+const SidebarComponent = () => {
+  return (
+    <>
+      <Row className='Sidebar p-4 '>
+        <Col className='d-flex flex-column row-gap-2'>
+            <div className="img d-flex justify-content-center">
+                <img className='Iconimg ' src={sumagoIcon} alt="img not Found" />
+            </div>
+            <div as={NavLink} className='profileName m-4 d-flex column-gap-3 ' >
+               <img className='Profile-Pic' src={ProfilePic} />
+                Vivek Sanjay Patil
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <SlHome className='me-3 fs-5 text-primary'/>Overview
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <IoAlarmOutline className='me-2 fs-4 text-danger'/> My Leave
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <IoKeyOutline className='me-2 fs-4 text-success'/>    My Attendace
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <LuFileSpreadsheet className='me-2 fs-4 text-success' /> My Timesheet
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <MdPendingActions className='me-2 fs-4 text-primary'/>    My Compensation
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <FiTrendingUp className='me-2 fs-4 text text-secondary'/>    My Performance
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <MdOutlinePersonSearch className='me-2 fs-3 text-primary'/>    Recruitment
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <SlPeople className='me-2 fs-4 text-success'/>    External Recruiter
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <TbTopologyRing className='me-2 fs-4 text-warning'/>    Intranet
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <MdDevicesOther className='me-2 fs-4 text-Secondary'/>    My Assets
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <CiLogout className='me-2 fs-4 text-warning'/>    My Exit
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <FaRegBell className='me-2 fs-4 text-danger'/>    Alerts
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <SlCalender className='me-2 fs-4 text-info'/>    My Calender
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <FaPeopleRobbery className='me-2 fs -4 text-success'/>    People
+            </div>
+            <div as={NavLink} className='Sidebar-menu'>
+            <GiLevelFourAdvanced className='me-2fs-4 text-primary'/>    Knowledge Base
+            </div>
+        </Col>
+      </Row>
+    </>
+  )
+}
+
+export default SidebarComponent
