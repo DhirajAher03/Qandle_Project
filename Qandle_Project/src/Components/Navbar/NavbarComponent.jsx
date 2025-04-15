530a9c8eccb102919879f35b468dfe5c9602c07d
Dhiraj Aher <dhirajaheraher@gmail.com>
2025-04-15 10:27:22 +0530

    Shubham Code Pulled

--- Qandle_Project/src/Components/Navbar/NavbarComponent.jsx
+++ Qandle_Project/src/Components/Navbar/NavbarComponent.jsx
@@ -1,116 +1,161 @@
-import { Navbar, Nav, Form, FormControl, Dropdown, InputGroup, Container, } from 'react-bootstrap';
-import { Gear, PersonCircle, QuestionCircle, BoxArrowRight, Search, } from 'react-bootstrap-icons';
+import React, { useState } from 'react';
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Dropdown,
+  InputGroup,
+  Container,
+  Modal,
+  Button,
+} from 'react-bootstrap';
+import {
+  Gear,
+  PersonCircle,
+  QuestionCircle,
+  BoxArrowRight,
+  Search,
+} from 'react-bootstrap-icons';
 import { HiBellAlert } from "react-icons/hi2";
 import { LuShipWheel } from "react-icons/lu";
 import { BiLike } from "react-icons/bi";
 import { TiPrinter } from "react-icons/ti";
 import { GrDocumentText } from "react-icons/gr";
-import { FaBars } from 'react-icons/fa';
 
-import "./Navbar.css";
+import './Navbar.css';
 
-const NavbarComponent = ({ toggleSidebar }) => {
+const NavbarComponent = () => {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
     <>
-      <Navbar expand="lg" className="bg-primary shadow-sm p-0 position-fixed w-100 z-1">
-        <Container fluid className="px-4">
-          {/* Brand and Toggle */}
-          <Navbar.Brand href="#home" className="fw-semibold logo-text fs-2 text-white">Qandle</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbar-nav" className="bg-white rounded" />
+      <Navbar className="bg-primary shadow-sm p-2 position-fixed w-100 z-3">
+        <Container fluid className="d-flex align-items-center justify-content-between px-3">
+          {/* Brand */}
+          
+          <Navbar.Brand href="#home" className="fw-semibold fs-4 text-white m-0">
+            Qandle
+          </Navbar.Brand>
 
-          {/* Collapse content */}
-          <Navbar.Collapse id="navbar-nav">
-            {/*Search Bar */}
-            <div className="d-flex justify-content-center w-100 my-3 my-lg-0">
-              <Form style={{ maxWidth: '600px' }} className="w-100">
-                <InputGroup className="w-100">
-                  {/* Dropdown */}
-                  <Dropdown>
-                    <Dropdown.Toggle
-                      variant="light"
-                      id="category-dropdown"
-                      className="rounded-start-pill border-end-0 px-4 d-flex align-items-center gap-2"
-                    >
-                      People
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                      <Dropdown.Item>People</Dropdown.Item>
-                      <Dropdown.Item>Name</Dropdown.Item>
-                      <Dropdown.Item>Department</Dropdown.Item>
-                      <Dropdown.Item>Location</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
+          {/* Desktop Search Bar */}
+          <div className="d-none d-lg-block flex-grow-1 mx-4">
+            <Form style={{ maxWidth: '600px' }} className="w-100">
+              <InputGroup>
+                <InputGroup.Text className="bg-white">
+                  <Search />
+                </InputGroup.Text>
+                <FormControl
+                  type="search"
+                  placeholder="Search by name, department or location"
+                  className="px-3"
+                />
+              </InputGroup>
+            </Form>
+          </div>
 
-                  {/* Search Icon */}
-                  <InputGroup.Text className="bg-white border-start-0 border-end-0">
-                    <Search />
-                  </InputGroup.Text>
+          {/* Mobile View Buttons */}
+          <div className="d-flex align-items-center gap-3 ms-auto d-lg-none">
+            <Nav.Link
+              onClick={() => setShowSearch(true)}
+              className="bg-white rounded-circle d-flex align-items-center justify-content-center"
+              style={{ height: '35px', width: '35px' }}
+            >
+              <Search size={18} className="text-dark" />
+            </Nav.Link>
 
-                  {/* Input Field */}
-                  <FormControl
-                    type="search"
-                    placeholder="Search by name, department or location"
-                    className="border-start-0 border-end-0 px-3 rounded-end-pill"
-                    aria-label="Search"
-                  />
+            <Nav.Link
+              href="#notifications"
+              className="bg-white rounded-circle d-flex align-items-center justify-content-center"
+              style={{ height: '35px', width: '35px' }}
+            >
+              <HiBellAlert size={18} className="text-dark" />
+            </Nav.Link>
+          </div>
 
-                </InputGroup>
-              </Form>
-            </div>
+          {/* Notification for Desktop */}
+          <Nav.Link
+            href="#notifications"
+            className="bg-white rounded-circle d-none d-lg-flex align-items-center justify-content-center ms-3"
+            style={{ height: '40px', width: '40px' }}
+          >
+            <HiBellAlert size={20} className="text-dark" />
+          </Nav.Link>
 
-            {/* Right Icons */}
-            <Nav className="align-items-center gap-3 justify-content-center mt-3 mt-lg-0 ms-lg-auto">
-              <Nav.Link
-                href="#notifications"
-                className="bg-white rounded-circle d-flex align-items-center justify-content-center"
-                style={{ height: '40px', width: '40px' }}
-              >
-                <HiBellAlert size={20} className="text-dark" />
-              </Nav.Link>
-
-              <Dropdown align="end">
-                <Dropdown.Toggle variant="light" id="dropdown-basic" className="d-flex align-items-center icon-rounded text-secondary border-0">
-                  <PersonCircle size={35} className="me-1" />
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#setup">
-                    <LuShipWheel size={18} className="me-2 text-info" />
-                    Set Up Settings
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#account">
-                    <Gear size={16} className="me-2" />
-                    Account Settings
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#favourites">
-                    <BiLike size={18} className="me-2 text-warning" />
-                    Add to Favourites
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#report">
-                    <TiPrinter size={18} className="me-2 text-success" />
-                    Report a Problem
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#report">
-                    <QuestionCircle size={16} className="me-2 text-danger" />
-                    Product Tour
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#report">
-                    <GrDocumentText size={18} className="me-2 text-primary" />
-                    Help Text Visible
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#report">
-                    <QuestionCircle size={16} className="me-2 text-warning" />
-                    CS Session
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#logout">
-                    <BoxArrowRight size={16} className="me-2 text-danger" />
-                    Logout
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Nav>
-          </Navbar.Collapse>
+          {/* Profile Dropdown (same for all views) */}
+          <Dropdown align="end" className="ms-2">
+            <Dropdown.Toggle
+              variant="light"
+              id="dropdown-basic"
+              className="d-flex align-items-center icon-rounded text-secondary border-0"
+            >
+              <PersonCircle size={35} className="me-1" />
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="#setup">
+                <LuShipWheel size={18} className="me-2 text-info" />
+                Set Up Settings
+              </Dropdown.Item>
+              <Dropdown.Item href="#account">
+                <Gear size={16} className="me-2" />
+                Account Settings
+              </Dropdown.Item>
+              <Dropdown.Item href="#favourites">
+                <BiLike size={18} className="me-2 text-warning" />
+                Add to Favourites
+              </Dropdown.Item>
+              <Dropdown.Item href="#report">
+                <TiPrinter size={18} className="me-2 text-success" />
+                Report a Problem
+              </Dropdown.Item>
+              <Dropdown.Item href="#report">
+                <QuestionCircle size={16} className="me-2 text-danger" />
+                Product Tour
+              </Dropdown.Item>
+              <Dropdown.Item href="#report">
+                <GrDocumentText size={18} className="me-2 text-primary" />
+                Help Text Visible
+              </Dropdown.Item>
+              <Dropdown.Item href="#report">
+                <QuestionCircle size={16} className="me-2 text-warning" />
+                CS Session
+              </Dropdown.Item>
+              <Dropdown.Item href="#logout">
+                <BoxArrowRight size={16} className="me-2 text-danger" />
+                Logout
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Container>
       </Navbar>
+
+      {/* Floating Search Modal for Mobile */}
+      <Modal
+        show={showSearch}
+        onHide={() => setShowSearch(false)}
+        centered
+        size="lg"
+        className="search-modal"
+      >
+        <Modal.Body className="p-3">
+          <Form className="w-100">
+            <InputGroup>
+              <InputGroup.Text className="bg-white">
+                <Search />
+              </InputGroup.Text>
+              <FormControl
+                type="search"
+                placeholder="Search by name, department or location"
+                className="px-3"
+                autoFocus
+              />
+              <Button variant="secondary" onClick={() => setShowSearch(false)}>
+                Close
+              </Button>
+            </InputGroup>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
--- Qandle_Project/src/Components/Overview/TimeAndAttendanceComponent.jsx
+++ Qandle_Project/src/Components/Overview/TimeAndAttendanceComponent.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Overview.css';
-import { Card, Dropdown, Button, Row, Col , Container } from 'react-bootstrap';
+import { Card, Dropdown, Button, Row, Col, Container } from 'react-bootstrap';
 import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, } from 'chart.js';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
 
-// Register chart elements
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const TimeAndAttendenceComponent = () => {
+  const [currentTime, setCurrentTime] = useState('00:00:00');
+  const [breakTime, setBreakTime] = useState(0);
+  const [workingTime, setWorkingTime] = useState(0);
+  const [isBreak, setIsBreak] = useState(false);
+  const [isClockedOut, setIsClockedOut] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date().toLocaleTimeString());
+      if (!isClockedOut && !isBreak) setWorkingTime((prev) => prev + 1);
+      if (!isClockedOut && isBreak) setBreakTime((prev) => prev + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [isBreak, isClockedOut]);
+
+  const formatTime = (seconds) => {
+    const h = String(Math.floor(seconds / 3600)).padStart(2, '0');
+    const m = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
+    const s = String(seconds % 60).padStart(2, '0');
+    return `${h}:${m}:${s}`;
+  };
+
   const data = {
     labels: ['07 Mon', '08 Tue', '09 Wed', '10 Thu', '11 Fri', '12 Sat', '13 Sun'],
     datasets: [
@@ -15,16 +44,19 @@ const TimeAndAttendenceComponent = () => {
         label: 'Break Duration',
         backgroundColor: '#fbc02d',
         data: [0, 0, 0, 0, 0, 0, 0],
+        barThickness: 20,
       },
       {
         label: 'Work Hours',
         backgroundColor: '#4caf50',
         data: [1.81, 0, 0, 0, 0, 0, 0],
+        barThickness: 20,
       },
       {
         label: 'Auto ClockOut',
         backgroundColor: '#f44336',
         data: [0, 0, 0, 0, 0, 0, 0],
+        barThickness: 20,
       },
     ],
   };
@@ -40,42 +72,47 @@ const TimeAndAttendenceComponent = () => {
       y: {
         beginAtZero: true,
       },
+      x: {
+        stacked: true,
+        categoryPercentage: 0.6,
+        barPercentage: 0.8,
+      },
     },
   };
 
   return (
-    <>
-      <Container className='p-3'>
-        <h4 className="fw-bold mb-3">Overview</h4>
-        <Card className="p-3 box-shadow rounded-4 border-0">
-          <h5 className="fw-bold mb-3">Time & Attendance</h5>
-          <Row className="custom-row">
-            {/* Left Part: Time Data */}
-            <Col md={6} className="border-end">
-              <div className="d-flex flex-wrap align-items-center justify-content-between gap-3">
-                <div className="text-center mx-auto mx-md-0">
-                  <h1 className="text-primary fw-bold">00:33:36</h1>
-                  <p>07-Apr-2025</p>
-                </div>
+    <Container className='p-3'>
+      <h4 className="fw-bold mb-3 text-center text-md-start">Overview</h4>
+      <Card className="p-3 box-shadow rounded-4 border-0">
+        <h5 className="fw-bold mb-3 text-center text-md-start">Time & Attendance</h5>
+        <Row className="custom-row">
+          <Col md={6} className="border-end mb-4 mb-md-0">
+            <div className="d-flex flex-column align-items-center gap-3">
+              <div className="text-center">
+                <h1 className="text-primary fw-bold time-display">{formatTime(workingTime)}</h1>
+                <p>{new Date().toLocaleDateString('en-GB')}</p>
+              </div>
 
-                <div className="d-flex flex-wrap gap-3 justify-content-center p-3">
-                  <div className="bg-light rounded p-2 text-center">
-                    <p className="mb-1 fw-bold">09:03:07</p>
-                    <small className="text-muted">Clock In Time</small>
-                  </div>
-                  <div className="bg-warning rounded p-2 text-center">
-                    <p className="mb-1 fw-bold">00:00:00</p>
-                    <small className="text-muted">Break Duration</small>
-                  </div>
+              <div className="d-flex flex-wrap gap-3 justify-content-center">
+                <div className="bg-light rounded p-2 text-center">
+                  <p className="mb-1 fw-bold">09:03:07</p>
+                  <small className="text-muted">Clock In Time</small>
                 </div>
-
-                <div className="d-flex gap-3 justify-content-center">
-                  <Button variant="warning">Start Break</Button>
-                  <Button variant="danger">Clock Out</Button>
+                <div className="bg-warning rounded p-2 text-center">
+                  <p className="mb-1 fw-bold">{formatTime(breakTime)}</p>
+                  <small className="text-muted">Break Duration</small>
                 </div>
               </div>
 
-              {/* Period Dropdown */}
+              <div className="d-flex gap-3 justify-content-center">
+                <Button variant="warning" onClick={() => setIsBreak((prev) => !prev)}>
+                  {isBreak ? 'End Break' : 'Start Break'}
+                </Button>
+                <Button variant="danger" onClick={() => setIsClockedOut(true)}>
+                  Clock Out
+                </Button>
+              </div>
+
               <div className="mt-4 text-center">
                 <div className="Period-border">
                   <span className="fw-semibold">Period:</span>
@@ -87,15 +124,12 @@ const TimeAndAttendenceComponent = () => {
                 </div>
               </div>
 
-              {/* Time Cards with Arrows */}
               <div className="mt-3">
                 <div className="d-flex justify-content-center align-items-center gap-3 flex-wrap">
-                  {/* Left Arrow */}
                   <Button variant="white" className="rounded-circle px-2 py-1 d-none d-md-inline-block">
                     &#8592;
                   </Button>
 
-                  {/* Cards */}
                   <div className="bg-light rounded-pill px-4 py-3 text-center shadow-sm">
                     <h5 className="mb-0 fw-bold text-primary">10:16</h5>
                     <small className="fw-semibold">Average Working Hours</small>
@@ -105,28 +139,24 @@ const TimeAndAttendenceComponent = () => {
                     <small className="fw-semibold">Average Break Duration</small>
                   </div>
 
-                  {/* Right Arrow */}
                   <Button variant="white" className="rounded-circle px-2 py-1 d-none d-md-inline-block">
                     &#8594;
                   </Button>
                 </div>
               </div>
-            </Col>
+            </div>
+          </Col>
 
-            {/* Right Part: Bar Chart */}
-            <Col md={6}>
-              <div>
-                <Bar data={data} options={options} height={150} />
-                <div className="d-flex justify-content-between mt-2 text-muted">
-                  <span>Pre Week</span>
-                  <span>Next Week</span>
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </Card>
-      </Container>
-    </>
+          <Col md={6} className="d-flex flex-column align-items-center">
+            <Bar data={data} options={options} height={280} />
+            <div className="d-flex justify-content-between w-100 mt-2 text-muted px-3">
+              <span>Pre Week</span>
+              <span>Next Week</span>
+            </div>
+          </Col>
+        </Row>
+      </Card>
+    </Container>
   );
 };
 
